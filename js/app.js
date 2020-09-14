161ec771d52ff33816c7d7d55df0e57aa5cc92a2
Ahmad <almohammedahmed2@gmail.com>
2020-09-15 00:20:31 +0300

    declaring object using constructor and adding table

--- js/app.js
+++ js/app.js
@@ -1,354 +1,162 @@
 'use strict';
+// Declaring global variables
+var Arr1 = [];
+var LEN = 15;
+var cumArr = new Array(LEN).fill(0);
+var div1 = document.getElementById('unorderedList');
+var table1 = document.createElement('table');
+div1.appendChild(table1);
 
-var Seattle = {
-    Name: 'Seattle',
-    MinHourlyCustomers: 23,
-    MaxHourlyCustomers: 65,
-    AverageCookiesPerCustomer: 6.3,
-    Arr1: [],
-//  declaring method picking random number between the Max and Min
-    randomNumber: function () {
-        var random = Math.random();
-        random = this.MinHourlyCustomers + random * (this.MaxHourlyCustomers - this.MinHourlyCustomers + 1);
-        random = Math.floor(random);
+// Declaring function for objacts
+function Cookies(name, MinHourlyCustomers, MaxHourlyCustomers, AverageCookiesPerCustomer) {
+    this.Name = name;
+    this.MinHourlyCustomers = MinHourlyCustomers;
+    this.MaxHourlyCustomers = MaxHourlyCustomers;
+    this.AverageCookiesPerCustomer = AverageCookiesPerCustomer;
+}
+// Declaring a method to get a random number betwenn two values
+Cookies.prototype.randomNumber = function () {
+    var random = Math.random();
+    random = this.MinHourlyCustomers + random * (this.MaxHourlyCustomers - this.MinHourlyCustomers + 1);
+    random = Math.floor(random);
 
-        return random;
-        
-    },
-    //  declaring method to round the number to intiger number equals or less 
-    averagePurchased: function () {
-        var AverageCookies = Math.floor(this.AverageCookiesPerCustomer * this.randomNumber());
+    return random;
 
-        return AverageCookies;
-    },
-    //  declaring method to add elemnts to array
-    finalResults: function () {
-        for (var i = 0; i < 14; i++) {
-            this.Arr1.push(this.averagePurchased());
+}
+// Declaring method to get random average purchased cookies per hour
+Cookies.prototype.averagePurchased = function () {
+    var AverageCookies = Math.floor(this.AverageCookiesPerCustomer * this.randomNumber());
 
-        }
-
+    return AverageCookies;
+}
+// Declaring method to repeat last method to have 14 random number
+Cookies.prototype.finalResults = function () {
+    Arr1 = [];
+    for (var i = 0; i < 14; i++) {
+        Arr1.push(this.averagePurchased());
+        cumArr[i] +=  Arr1[i];
     }
 
-};
-Seattle.finalResults();
-
-//  creating elements 
-
-var x = document.getElementById('unorderedList');
-var y = document.createElement('h2');
-x.appendChild(y);
-y.textContent = 'Seattle';
-
-var list = document.createElement('ul');
-x.appendChild(list);
-var strFlag = 'am';
-for (var i = 0; i < 14; i++) {
-    var list_ = document.createElement('li');
-    list.appendChild(list_);
-    j = i + 6;
-    if (j >= 12) strFlag = 'pm';
-    if (j > 12) { j -= 12; }
-
-    list_.textContent = j + strFlag + ': ' + Seattle.Arr1[i] + ' cookies';
-}
-
-//  creating total 
-var summSeattle = 0;
-for (var j = 0; j < Seattle.Arr1.length; j++) {
-    summSeattle += Seattle.Arr1[j];
-
-}
-var chList15 = document.createElement('li');
-list.appendChild(chList15);
-chList15.textContent = 'Total: ' + summSeattle + ' cookies';
-
-
-
-
-
-//  Tokyo location ___________________________________________________________________________________________________
-
-
-
-var Tokyo = {
-    Name: 'Toyo',
-    MinHourlyCustomers: 3,
-    MaxHourlyCustomers: 24,
-    AverageCookiesPerCustomer: 1.2,
-    Arr1: [],
-//  declaring method to pick a random number between the Max number and Min number 
-    randomNumber: function () {
-        var random = Math.random();
-        random = this.MinHourlyCustomers + random * (this.MaxHourlyCustomers - this.MinHourlyCustomers + 1);
-        random = Math.floor(random);
-
-        return random;
-    },
-     //  declaring method to round the number to intiger number equals or less 
-    averagePurchased: function () {
-        var AverageCookies = Math.floor(this.AverageCookiesPerCustomer * this.randomNumber());
-
-        return AverageCookies;
-    },
-    //  declaring method to add elemnts to array
-    finalResults: function () {
-        for (var i = 0; i < 14; i++) {
-            this.Arr1.push(this.averagePurchased());
-
-        }
-
-    }
-
-};
-Tokyo.finalResults();
-
-//  creating elements
-var x = document.getElementById('unorderedList');
-var y = document.createElement('h2');
-x.appendChild(y);
-y.textContent = 'Tokyo';
-
-var list = document.createElement('ul');
-x.appendChild(list);
-
-var list = document.createElement('ul');
-x.appendChild(list);
-
-var strFlag = 'am';
-for (var i = 0; i < 14; i++) {
-    var list_ = document.createElement('li');
-    list.appendChild(list_);
-    j = i + 6;
-    if (j >= 12) strFlag = 'pm';
-    if (j > 12) { j -= 12; }
-
-    list_.textContent = j + strFlag + ': ' + Tokyo.Arr1[i] + ' cookies';
-}
-
-//  creating total 
-var summTokyo = 0;
-for (var j = 0; j < Tokyo.Arr1.length; j++) {
-    summTokyo += Tokyo.Arr1[j];
-
-}
-var chList15 = document.createElement('li');
-list.appendChild(chList15);
-chList15.textContent = 'Total: ' + summTokyo + ' cookies';
-
-
-
-
-
-
-
-
-//  Dubai location ___________________________________________________________________________________________________
-
-var Dubai = {
-    Name: 'Dubai',
-    MinHourlyCustomers: 11,
-    MaxHourlyCustomers: 38,
-    AverageCookiesPerCustomer: 3.7,
-    Arr1: [],
-
-//  declaring method to pick a random number between the Max number and Min number 
-
-    randomNumber: function () {
-        var random = Math.random();
-        random = this.MinHourlyCustomers + random * (this.MaxHourlyCustomers - this.MinHourlyCustomers + 1);
-        random = Math.floor(random);
-
-        return random;
-    },
-
-    //  declaring method to round the number to intiger number equals or less
-    averagePurchased: function () {
-        var AverageCookies = Math.floor(this.AverageCookiesPerCustomer * this.randomNumber());
-
-        return AverageCookies;
-    },
-    //  declaring method to add elemnts to array
-    finalResults: function () {
-        for (var i = 0; i < 14; i++) {
-            this.Arr1.push(this.averagePurchased());
-
-        }
-
-    }
-
-};
-Dubai.finalResults();
-
-//  creating elements
-var x = document.getElementById('unorderedList');
-var y = document.createElement('h2');
-x.appendChild(y);
-y.textContent = 'Dubai';
-
-var list = document.createElement('ul');
-x.appendChild(list);
-
-var strFlag = 'am';
-for (var i = 0; i < 14; i++) {
-    var list_ = document.createElement('li');
-    list.appendChild(list_);
-    j = i + 6;
-    if (j >= 12) strFlag = 'pm';
-    if (j > 12) { j -= 12; }
-
-    list_.textContent = j + strFlag + ': ' + Dubai.Arr1[i] + ' cookies';
-}
-//  creating total
-var summDubai = 0;
-for (var j = 0; j < Dubai.Arr1.length; j++) {
-    summDubai += Dubai.Arr1[j];
-
-}
-var chList15 = document.createElement('li');
-list.appendChild(chList15);
-chList15.textContent = 'Total: ' + summDubai + ' cookies';
-
-
-
-
-
-
-
-//  Paris location ___________________________________________________________________________________________________
-
-
-var Paris = {
-    Name: 'Paris',
-    MinHourlyCustomers: 20,
-    MaxHourlyCustomers: 38,
-    AverageCookiesPerCustomer: 2.3,
-    Arr1: [],
-
-    //  declaring method to pick a random number between the Max number and Min number 
-
-    randomNumber: function () {
-        var random = Math.random();
-        random = this.MinHourlyCustomers + random * (this.MaxHourlyCustomers - this.MinHourlyCustomers + 1);
-        random = Math.floor(random);
-
-        return random;
-    },
-    //  declaring method to round the number to intiger number equals or less
-    averagePurchased: function () {
-        var AverageCookies = Math.floor(this.AverageCookiesPerCustomer * this.randomNumber());
-
-        return AverageCookies;
-    },
-    //  declaring method to add elemnts to array
-    finalResults: function () {
-        for (var i = 0; i < 14; i++) {
-            this.Arr1.push(this.averagePurchased());
-
-        }
-
-    }
-
-};
-Paris.finalResults();
-
-//  creating elements
-var x = document.getElementById('unorderedList');
-var y = document.createElement('h2');
-x.appendChild(y);
-y.textContent = 'Paris';
-
-var list = document.createElement('ul');
-x.appendChild(list);
-
-var strFlag = 'am'; 
-for (var i =0 ; i<14;i++){
-    var list_ = document.createElement('li');
-    list.appendChild(list_);
-    j = i+6;
-    if(j>=12)strFlag='pm';
-    if(j>12){j-=12;}
     
-    list_.textContent = j+strFlag+': '+ Paris.Arr1[i] +' cookies';
 }
-//  creating total
-var summParis = 0;
-for (var j = 0; j < Paris.Arr1.length; j++) {
-    summParis += Paris.Arr1[j];
-
-}
-var chList15 = document.createElement('li');
-list.appendChild(chList15);
-chList15.textContent = 'Total: ' + summParis + ' cookies';
 
 
+// Declaring method to creat table which have the random numbers and the summition of each row
+Cookies.prototype.render = function (para) {
+    this.row2 = document.createElement('tr');
+    table1.appendChild(this.row2);
 
 
+    this.sumRows = 0;
 
-
-//  Paris location ___________________________________________________________________________________________________
-
-
-var Lima = {
-    Name: 'Lima',
-    MinHourlyCustomers: 2,
-    MaxHourlyCustomers: 16,
-    AverageCookiesPerCustomer: 4.6,
-    Arr1: [],
-    //  declaring method to pick a random number between the Max number and Min number 
-
-    randomNumber: function () {
-        var random = Math.random();
-        random = this.MinHourlyCustomers + random * (this.MaxHourlyCustomers - this.MinHourlyCustomers + 1);
-        random = Math.floor(random);
-
-        return random;
-    },
-
-    //  declaring method to round the number to intiger number equals or less
-    averagePurchased: function () {
-        var AverageCookies = Math.floor(this.AverageCookiesPerCustomer * this.randomNumber());
-
-        return AverageCookies;
-    },
-
-    //  declaring method to add elemnts to array
-    finalResults: function () {
-        for (var i = 0; i < 14; i++) {
-            this.Arr1.push(this.averagePurchased());
-
+    
+    for (var i = 0; i <= 15; i++) {
+        if (i < 15) {
+            if(i==0){
+                this.list = document.createElement('td');
+                this.row2.appendChild(this.list);
+                this.list.textContent = this.Name;
+                
+            }
+            else{
+            this.list = document.createElement('td');
+            this.row2.appendChild(this.list);
+            this.list.textContent = Arr1[i-1];
+            this.sumRows += Arr1[i-1];}
         }
+        if (i == 15) {
+            this.list = document.createElement('td');
+            this.row2.appendChild(this.list);
+            this.list.textContent = this.sumRows;
+            cumArr [14]+=this.sumRows;
+        }
+
 
     }
 
-};
-Lima.finalResults();
-
-//  creating elements
-var x = document.getElementById('unorderedList');
-var y = document.createElement('h2');
-x.appendChild(y);
-y.textContent = 'Lima';
-
-var list = document.createElement('ul');
-x.appendChild(list);
-
-var strFlag = 'am'; 
-for (var i =0 ; i<14;i++){
-    var list_ = document.createElement('li');
-    list.appendChild(list_);
-    j = i+6;
-    if(j>=12)strFlag='pm';
-    if(j>12){j-=12;}
-    
-    list_.textContent = j+strFlag+': '+ Lima.Arr1[i] +' cookies';
-}
-//  creating total
-var summLima = 0;
-for (var j = 0; j < Lima.Arr1.length; j++) {
-    summLima += Lima.Arr1[j];
 
 }
-var chList15 = document.createElement('li');
-list.appendChild(chList15);
-chList15.textContent = 'Total: ' + summLima + ' cookies';
+
+// Declaring method to creat last row which have the summition of each column
+Cookies.prototype.renderSum = function(){
+    this.lastRow= document.createElement('tr');
+    table1.appendChild(this.lastRow);
+     for(var i = 0 ; i <16 ; i++){
+         if(i==0)
+         {
+            this.sumCol = document.createElement('td');
+            this.lastRow.appendChild(this.sumCol);
+            this.sumCol.textContent= 'Totals'; 
+         }
+         else{
+            this.sumCol = document.createElement('td');
+            this.lastRow.appendChild(this.sumCol);
+            this.sumCol.textContent= cumArr[i-1]; 
+        }
+     }
+
+
+
+}
+
+
+// Creating the titel row 
+var tableTitel = document.createElement('tr');
+table1.appendChild(tableTitel);
+
+var strFlag = 'am';
+for (var i = 0; i < 16; i++) {
+    if (i == 0) {
+        var emptyCell = document.createElement('td');
+        tableTitel.appendChild(emptyCell);
+        emptyCell.textContent = '\t';
+
+    }
+
+
+    else if (i == 15) {
+        var emptyCell = document.createElement('td');
+        tableTitel.appendChild(emptyCell);
+        emptyCell.textContent = 'Daily Location Total';
+
+    }
+    else {
+        var emptyCell = document.createElement('td');
+        tableTitel.appendChild(emptyCell);
+        var j = i + 5;
+        if (j >= 12) strFlag = 'pm';
+        if (j > 12) { j -= 12; }
+        emptyCell.textContent = j + ':00' + strFlag
+    }
+
+
+}
+
+
+var seattle = new Cookies('Seattle', 23, 65, 6.3);
+var tokyo = new Cookies('Tokyo', 3, 24, 1.2);
+var dubai = new Cookies('Dubai', 11, 38, 3.7);
+var paris = new Cookies('Paris', 20, 38, 2.3);
+var lima = new Cookies('Lima', 2, 16, 4.6);
+
+
+
+seattle.finalResults();
+seattle.render();
+
+tokyo.finalResults();
+tokyo.render();
+
+dubai.finalResults();
+dubai.render();
+
+paris.finalResults();
+paris.render();
+
+lima.finalResults();
+lima.render();
+lima.renderSum();
+
+
+
+
